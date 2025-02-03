5642834f37c2babdb9343877faa51cfdd833f649
Wemerson Damasceno <wemerson.ebm@lojadomecanico.com.br>
2025-02-03 17:53:48 -0300

    feat: create list task view and widgets

---
+++ lib/src/list_task/presentation/view/list_task_view.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:taski/src/core/constants/app_colors.dart';
+import 'package:taski/src/list_task/presentation/view_model/task_view_model.dart';
+import 'package:taski/src/list_task/presentation/widgets/task_item_widget.dart';
+
+class TaskListView extends StatelessWidget {
+  const TaskListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.backgroundScreen,
+      appBar: AppBar(
+        backgroundColor: AppColors.backgroundScreen,
+        surfaceTintColor: AppColors.backgroundScreen,
+        title: const Row(
+          children: [
+            Icon(Icons.check_box_sharp, color: AppColors.primaryColor),
+            Text("Taski"),
+          ],
+        ),
+        centerTitle: false,
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 16),
+            child: Row(
+              children: [
+                Text("John"),
+                SizedBox(width: 10),
+                CircleAvatar(
+                  backgroundImage: NetworkImage(
+                    "https://images.pexels.com/photos/1040880/pexels-photo-1040880.jpeg?auto=compress&cs=tinysrgb&w=800",
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            RichText(
+              text: const TextSpan(
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+                children: [
+                  TextSpan(
+                    text: "Welcome, ",
+                    style: TextStyle(
+                      color: AppColors.secondaryColor,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "John.",
+                    style: TextStyle(
+                      color: AppColors.primaryColor,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 8),
+            const Text(
+              "You've got 7 tasks to do.",
+              style: TextStyle(
+                color: AppColors.descriptionColor,
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(height: 24),
+            Expanded(
+              child: Consumer<TaskViewModel>(
+                builder: (context, taskViewModel, child) {
+                  return ListView.builder(
+                    itemCount: taskViewModel.tasks.length,
+                    itemBuilder: (context, index) {
+                      return TaskItemWidget(
+                        task: taskViewModel.tasks[index],
+                        onToggle: () => taskViewModel.toggleTaskStatus(index),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        enableFeedback: true,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.ballot_outlined),
+            label: "Todo",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_box_outlined),
+            label: "Create",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: "Search",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.check_box_outlined),
+            label: "Done",
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/src/list_task/presentation/widgets/task_item_widget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:taski/src/core/constants/app_colors.dart';
+import 'package:taski/src/list_task/presentation/models/task_model.dart';
+
+class TaskItemWidget extends StatelessWidget {
+  final TaskModel task;
+  final VoidCallback onToggle;
+
+  const TaskItemWidget({super.key, required this.task, required this.onToggle});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0,
+      margin: const EdgeInsets.only(right: 8, top: 8, bottom: 8),
+      color: AppColors.backgroundCard,
+      child: ListTile(
+        contentPadding: EdgeInsets.zero,
+        titleAlignment: ListTileTitleAlignment.top,
+        leading: Checkbox(
+          visualDensity: VisualDensity.compact,
+          activeColor: AppColors.primaryColor,
+          value: task.isCompleted,
+          onChanged: (_) => onToggle(),
+        ),
+        title: Text(
+          task.title,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            color: AppColors.secondaryColor,
+          ),
+        ),
+        subtitle: task.description.isNotEmpty
+            ? Text(
+                task.description,
+                style: const TextStyle(color: AppColors.descriptionColor),
+              )
+            : null,
+        trailing: IconButton(
+          icon: const Icon(
+            Icons.more_horiz,
+            color: AppColors.descriptionColor,
+          ),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
