b9435710dfd99a8b07069d603907a43ce1e0c001
Wemerson Damasceno <wemerson.ebm@lojadomecanico.com.br>
2025-02-03 17:52:58 -0300

    feat: create constants AppColors

---
+++ lib/src/core/constants/app_colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF027fff);
+  static const Color secondaryColor = Color(0xFF3f3d56);
+
+  static const backgroundScreen = Color(0xFFffffff);
+  static const backgroundCard = Color(0xFFf6f7f9);
+  static const descriptionColor = Color(0xFFa9b5ca);
+}
