969bdfb08a07296385b39275ca4162b052771256
Wemerson Damasceno <wemerson.ebm@lojadomecanico.com.br>
2025-02-04 14:41:28 -0300

    feat: change value-notifier for bloc

---
+++ lib/src/list_task/presentation/bloc/list_task_state.dart
@@ -0,0 +1,31 @@
+import 'package:equatable/equatable.dart';
+import 'package:taski/src/core/enums/state_enum.dart';
+import 'package:taski/src/list_task/presentation/models/task_model.dart';
+
+abstract class ListTaskState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class StateListTask extends ListTaskState {
+  final List<TaskModel>? tasks;
+  final StateEnum stateEnum;
+
+  StateListTask({
+    this.tasks,
+    this.stateEnum = StateEnum.initial,
+  });
+
+  StateListTask copyWith({
+    List<TaskModel>? tasks,
+    StateEnum? stateEnum,
+  }) {
+    return StateListTask(
+      tasks: tasks ?? this.tasks,
+      stateEnum: stateEnum ?? this.stateEnum,
+    );
+  }
+
+  @override
+  List<Object?> get props => [tasks, stateEnum];
+}
