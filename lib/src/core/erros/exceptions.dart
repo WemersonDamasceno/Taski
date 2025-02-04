1035691d63236bead6b87392c8483c5e9a15f9b4
Wemerson Damasceno <wemerson.ebm@lojadomecanico.com.br>
2025-02-04 14:40:09 -0300

    feat: add failures and exceptions files

---
+++ lib/src/core/erros/exceptions.dart
@@ -0,0 +1,9 @@
+class LocalStorageException implements Exception {
+  final String message;
+  LocalStorageException(this.message);
+}
+
+class GenericsException implements Exception {
+  final String message;
+  GenericsException(this.message);
+}
