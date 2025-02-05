95b2998006a55d6e0c5957ec032e96bb99dc14f7
Wemerson Damasceno <wemerson.ebm@lojadomecanico.com.br>
2025-02-05 14:12:00 -0300

    feat: add package svg and assets

--- lib/src/core/constants/app_images.dart
+++ lib/src/core/constants/app_images.dart
@@ -2,4 +2,9 @@ class AppImages {
   static const _pathBase = 'assets/images';
   static const errorImage = '$_pathBase/error_image.png';
   static const emptyImage = '$_pathBase/empty_image.png';
+
+  static const searchImage = '$_pathBase/search_icon.svg';
+  static const todo = '$_pathBase/todo.svg';
+  static const create = '$_pathBase/create.svg';
+  static const done = '$_pathBase/done.svg';
 }
