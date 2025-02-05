a1704e03f8cc7299a5979e9477821703b9b67c28
Wemerson Damasceno <wemerson.ebm@lojadomecanico.com.br>
2025-02-05 09:45:57 -0300

    feat: add logic to navigation in bottom-navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:taski/src/core/theme/theme_app.dart';
+import 'package:taski/src/home_navigation/presentation/views/home_navigation.dart';
 import 'package:taski/src/injection_container.dart';
-import 'package:taski/src/list_task/presentation/view/list_task_page.dart';
 
 void main() async {
   await initDependency();
@@ -16,7 +16,7 @@ class TaskiApp extends StatelessWidget {
     return MaterialApp(
       theme: AppTheme.lightTheme,
       debugShowCheckedModeBanner: false,
-      home: const ListTaskPage(),
+      home: const HomeNavigationPage(),
     );
   }
 }
