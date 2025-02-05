6715f9c8eeceb90a0f79c199891e9d8e21fb8dae
Wemerson Damasceno <wemerson.ebm@lojadomecanico.com.br>
2025-02-05 09:48:45 -0300

    feat: create the screen CreateTaskView

---
+++ lib/src/core/widgets/app_input_text_widget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:taski/src/core/constants/app_colors.dart';
+import 'package:taski/src/core/extensions/theme_extension.dart';
+
+class AppInputTextWidget extends StatelessWidget {
+  const AppInputTextWidget({
+    super.key,
+    required this.controller,
+    required this.hintText,
+  });
+
+  final TextEditingController controller;
+  final String hintText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: TextField(
+        cursorColor: AppColors.slateBlue,
+        controller: controller,
+        style: context.textTheme.titleSmall?.copyWith(
+          color: AppColors.slatePurple,
+        ),
+        maxLines: 4,
+        minLines: 1,
+        inputFormatters: [
+          LengthLimitingTextInputFormatter(100),
+        ],
+        decoration: InputDecoration(
+          hintText: hintText,
+          hintStyle: context.textTheme.titleSmall?.copyWith(
+            color: AppColors.slateBlue,
+          ),
+          border: InputBorder.none,
+        ),
+      ),
+    );
+  }
+}
--- lib/src/injection_container.dart
+++ lib/src/injection_container.dart
@@ -1,4 +1,7 @@
 import 'package:get_it/get_it.dart';
+import 'package:taski/src/core/widgets/button/bloc/button_cubit.dart';
+import 'package:taski/src/create_task/domain/usecase/create_task_usecase.dart';
+import 'package:taski/src/create_task/presentation/bloc/create_task_bloc.dart';
 import 'package:taski/src/list_task/domain/usecase/get_uncompleted_tasks_usecase.dart';
 import 'package:taski/src/list_task/presentation/bloc/list_task_bloc.dart';
 
@@ -7,9 +10,14 @@ final GetIt getIt = GetIt.instance;
 Future<void> initDependency() async {
   //****** Usecases ******//
   getIt.registerLazySingleton<FetchTasksUseCase>(() => FetchTasksUseCase());
+  getIt.registerLazySingleton<CreateTaskUsecase>(() => CreateTaskUsecase());
 
   //****** List Task ******//
   getIt.registerFactory<ListTaskBloc>(
     () => ListTaskBloc(fetchTasksUseCase: getIt()),
   );
+  getIt.registerFactory<CreateTaskBloc>(
+    () => CreateTaskBloc(createTaskUsecase: getIt()),
+  );
+  getIt.registerFactory<ButtonCubit>(() => ButtonCubit());
 }
