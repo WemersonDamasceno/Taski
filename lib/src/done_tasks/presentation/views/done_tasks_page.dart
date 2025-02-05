7610fbd21d55b1e9540931e3565691b36a0696ed
Wemerson Damasceno <wemerson.ebm@lojadomecanico.com.br>
2025-02-05 15:21:10 -0300

    feat: create screen for DoneTasksPage

--- lib/src/done_tasks/presentation/views/done_tasks_page.dart
+++ lib/src/done_tasks/presentation/views/done_tasks_page.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:taski/src/done_tasks/presentation/bloc/list_done_task_bloc.dart';
+import 'package:taski/src/done_tasks/presentation/views/done_tasks_view.dart';
 
 class DoneTasksPage extends StatelessWidget {
   const DoneTasksPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('DoneTasksPage'),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<ListDoneTaskBloc>(
+          create: (_) => GetIt.I.get<ListDoneTaskBloc>(),
+        ),
+      ],
+      child: const CompletedTasksView(),
     );
   }
 }
