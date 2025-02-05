fce3735aec9e733c3c6c9f330766f469564f98d1
Wemerson Damasceno <wemerson.ebm@lojadomecanico.com.br>
2025-02-05 15:21:32 -0300

    test: update tests adding id in TaskModel

--- lib/src/list_task/data/mocks/tasks_mocks.dart
+++ lib/src/list_task/data/mocks/tasks_mocks.dart
@@ -2,11 +2,34 @@ import 'package:taski/src/core/models/task_model.dart';
 
 final mockListTasks = [
   TaskModel(
-      title: "Design sign up flow",
-      description: "By the time a prospect arrives "),
-  TaskModel(title: "Design use case page", description: ""),
-  TaskModel(title: "Test Wireframe", description: ""),
-  TaskModel(title: "Create new task UI flow", description: ""),
-  TaskModel(title: "Collect project assets", description: ""),
-  TaskModel(title: "Collect Skills list", description: "", isCompleted: true),
+    id: 1,
+    title: "Design sign up flow",
+    description: "By the time a prospect arrives ",
+  ),
+  TaskModel(
+    id: 2,
+    title: "Design use case page",
+    description: "",
+  ),
+  TaskModel(
+    id: 3,
+    title: "Test Wireframe",
+    description: "",
+  ),
+  TaskModel(
+    id: 4,
+    title: "Create new task UI flow",
+    description: "",
+  ),
+  TaskModel(
+    id: 5,
+    title: "Collect project assets",
+    description: "",
+  ),
+  TaskModel(
+    id: 6,
+    title: "Collect Skills list",
+    description: "",
+    isCompleted: true,
+  ),
 ];
